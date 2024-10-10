b258683e554eab44f0bfdcaddddbb37f089ae0f4
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-09 22:29:08 -0600

    Create 10 de octubre.cpp

---
+++ PRACTICAS/10 de octubre.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+  
+  // EJERCICIO 1.1
+  // Variable tipo int que tenga el número de materias que estás cursando actualmente
+	int numeroMaterias;
+	cout << "Introduce el número de materias que estás cursando: ";
+	cin >> numeroMaterias;
+  cout << "Actualemente estás cursando " << numeroMaterias << " materias." << endl;
+  cout << endl;
+
+	// EJERCICIO 1.2 
+  // Variable tipo int que guarde el valor del cuatrimestre que estas cursando
+  int numeroCuatrimestre;
+  cout << "Introduce el número de cuatrimestre que cursas: ";
+  cin >> numeroCuatarimestre;
+  cout << "Estás cursando el " << numeroCuatrimestre << " cuatrimestre." << endl;
+  cout << endl;
+  
+	// EJERCICIO 1.3
+  // Variable string con identificador materia
+	string materia;
+	cout << "Introduce el nombre de la materia: ";
+	cin >> materia;
+	cout << "La materia que estas cursando es " << materia << "." << endl;
+  cout << endl;
+
+  // EJERCICIO 1.4
+  // Variable string con identificador cuenta
+  string cuenta;
+  cout << "Introduce tu número de cuenta de UNITEC: ";
+  cin >> cuenta;
+  cout << cuenta << "es tu número de cuenta." << endl;
+  cout << endl;
+
+  // EJERCICIO 1.5
+  // Variable string con identificador nombre 
+  string nombre;
+  cout << "Introduce tu nombre: ";
+  cin >> nombre;
+  cout << "Tu nombre es " << nombre << " ." << endl;
+  cout << endl;
+
+  // EJERCICIO 1.6
+  // Variable string con identificador apellidoPaterno
+  string apellidoPaterno;
+  cout << "Introduce tu apellido paterno: ";
+  cin >> apellidoPaterno;
+  cout << 
+
+}
