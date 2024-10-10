299bec24cb0674c5ade91811232ff462245a99a6
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-09 22:38:52 -0600

    Create 26 de septiembre.cpp

---
+++ PRACTICAS/26 de septiembre.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Declaración de un número entero 
+	int numeroEntero = 346;
+	cout << "Entero: " << numeroEntero << endl;
+
+	// Declaración de un número entero corto
+	short int numeroCorto = 54321;
+	cout << "Entero corto: " << numeroCorto << endl;
+
+	// Declaración de un número entero largo
+	long int numeroLargo = 1234567890;
+	cout << "Entero largo: " << numeroLargo << endl;
+
+	// Declaración de un número entero sin signo explícito
+	unsigned int numeroSinSigno = 600000000;
+	cout << "Entero sin signo: " << numeroSinSigno << endl;
+
+	// Declaración de un número entero corto sin signo explícito
+	unsigned short int numeroCortoSinSigno = 73425;
+	cout << "Entero corto sin signo: " << numeroCortoSinSigno << endl;
+
+	// Declaración de un número entero largo sin signo explícito
+	unsigned long int numeroLargoSinSigno = 90000000000;
+	cout << "Entero largo sin signo: " << numeroLargoSinSigno << endl;
+
+	// Declaración de un número entero con signo explícito
+	signed int numeroConSigno = -45;
+	cout << "Entero con signo: " << numeroConSigno << endl;
+
+	// Declaración de un número entero corto con signo explicito 
+	signed short int numeroCortoConSigno = -6387;
+	cout << "Entero corto con signo: " << numeroCortoConSigno << endl;
+
+	signed long int numeroLargoConSigno = -97361862738;
+	cout << "Entero largo con signo: " << numeroLargoConSigno << endl;
+
+	long long int numeroLargoLargo = 12345678912345678;
+	cout << "Entero largo largo; " << numeroLargoLargo << endl;
+
+
+}
