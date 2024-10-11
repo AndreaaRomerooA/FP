64d03d5da36f84db9462c3f9c3cb20e0ec1ccaeb
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-10 20:40:09 -0600

    Update Tarea 10 de octubre.cpp

--- PRACTICAS/Tarea 10 de octubre.cpp
+++ PRACTICAS/Tarea 10 de octubre.cpp
@@ -5,42 +5,79 @@ int main()
   
   // EJERCICIO 1.1
   // Variable tipo int que tenga el número de materias que estás cursando 
+  int numeroMaterias = 5;
+  cout << "Estoy cursando " << numeroMaterias << " materias." << endl; 
+  cout << endl;
 
   // EJERCICIO 1.2
   // Variable tipo int que tenga el valor del cuatrimestre que estás cursando
+  int numeroCuatrimestre = 4;
+  cout << "Estoy cursando el " << numeroCuatrimestre << " cuatrimestre." << endl;
+  cout << endl;
   
   // EJERCICIO 1.3
   // Variable tipo string con identificador materia
+  string materia = "Fundamentos de programación";
+  cout << "La materia que estoy cursando se llama " << materia << "." << endl;
+  cout << endl;
 
   // EJERCICIO 1.4
   // Variable tipo string con identificador cuenta
+  string cuenta = "335000566";
+  cout << "Mi número de cuenta es " << cuenta << endl;
+  cout << endl;
 
   // EJERCICIO 1.5
   // Variable tipo string con identificador nombre
+  string nombre = "Andrea";
+  cout << "Mi nombre es " << nombre << "." << endl;
+  cout << endl;
 
   // EJERCICIO 1.6
   // Variable tipo string con identificador apellidoPaterno
+  string apellidoPaterno;
+  cout << "Mi primer apellido es " << apellidoPaterno << "." << endl;
+  cout << endl;
 
   // EJERCICIO 1.7
   // Variable tipo string con identificador apellidoMaterno
+  string apellidoMaterno;
+  cout << "Mi segundo apellido es " << apellidoMaterno << "." << endl;
+  cout << endl;
 
   // EJERCICIO 1.8
   // Variable tipo long con identificador celular
+  long long celular = 5527388683;
+  cout << "Mi número de celular es " << celular << endl;
+  cout << endl;
 
   // EJERCICIO 1.9
   // Variable tipo long con identificador casa
+  long long casa = 5553607444;
+  cout << "El número de mi casa es " << casa << endl;
+  cout << endl;
 
   // EJERCICIO 1.10
   // Variable tipo string con identificador correoQueMasUso
+  string correoQueMasUso = "andyromero3839@gmail.com";
+  cout << correoQueMasUso << " es el correo que más uso." << endl;
+  cout << endl;
 
   // EJERCICIO 1.11
   // Variable tipo string con identificador correoMyUnitec
+  string correoMyUnitec = "andrea.romero35@my.unitec.edu.mx";
+  cout << "Mi correo de Unitec es " << correoMyUnitec << endl;
+  cout << endl;
 
   // EJERCICIO 1.12
   // Variable tipo string con identificador carrera
+  string carrera = "Diseño, animación y arte digital";
+  cout << "Actualmente estoy cursando la carrera de " << carrera << " en Unitec." << endl;
+  cout << endl;
 
   // EJERCICIO 1.13
   // Variable tipo bool con identificador blackboard
+  bool
 
   
 }
