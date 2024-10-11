76522905e4f66e7a3b455c919f2410dc3f74fc6f
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-10 19:10:48 -0600

    Update Tarea 10 de octubre.cpp

--- PRACTICAS/Tarea 10 de octubre.cpp
+++ PRACTICAS/Tarea 10 de octubre.cpp
@@ -4,31 +4,43 @@ int main()
 {
   
   // EJERCICIO 1.1
-  // Variable tipo int que 
+  // Variable tipo int que tenga el número de materias que estás cursando 
 
   // EJERCICIO 1.2
-
+  // Variable tipo int que tenga el valor del cuatrimestre que estás cursando
+  
   // EJERCICIO 1.3
+  // Variable tipo string con identificador materia
 
   // EJERCICIO 1.4
+  // Variable tipo string con identificador cuenta
 
   // EJERCICIO 1.5
+  // Variable tipo string con identificador nombre
 
   // EJERCICIO 1.6
+  // Variable tipo string con identificador apellidoPaterno
 
   // EJERCICIO 1.7
+  // Variable tipo string con identificador apellidoMaterno
 
   // EJERCICIO 1.8
+  // Variable tipo long con identificador celular
 
   // EJERCICIO 1.9
+  // Variable tipo long con identificador casa
 
   // EJERCICIO 1.10
+  // Variable tipo string con identificador correoQueMasUso
 
   // EJERCICIO 1.11
+  // Variable tipo string con identificador correoMyUnitec
 
   // EJERCICIO 1.12
+  // Variable tipo string con identificador carrera
 
   // EJERCICIO 1.13
+  // Variable tipo bool con identificador blackboard
 
   
 }
