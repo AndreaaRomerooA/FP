94be728bda31d1fed3a9fbd0198247eb5c7923dd
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-10 18:48:33 -0600

    Create Tarea 10 de octubre.cpp

---
+++ PRACTICAS/Tarea 10 de octubre.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+int main()
+{
+  
+  // EJERCICIO 1.1
+  // Variable tipo int que 
+
+  // EJERCICIO 1.2
+
+  // EJERCICIO 1.3
+
+  // EJERCICIO 1.4
+
+  // EJERCICIO 1.5
+
+  // EJERCICIO 1.6
+
+  // EJERCICIO 1.7
+
+  // EJERCICIO 1.8
+
+  // EJERCICIO 1.9
+
+  // EJERCICIO 1.10
+
+  // EJERCICIO 1.11
+
+  // EJERCICIO 1.12
+
+  // EJERCICIO 1.13
+
+  
+}
