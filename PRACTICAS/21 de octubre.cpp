a63dfa7df45a580e625be951dcd2ba56b95a0066
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-21 11:23:24 -0600

    Create 21 de octubre.cpp

---
+++ PRACTICAS/21 de octubre.cpp
@@ -0,0 +1,11 @@
+using namespace 
+int num = 10;
+	if (num % 2 == 0) // nunca poner ; en esta linea 
+	{
+		cout << "par " << endl;
+
+	}
+	else // nunca lleva condición
+	{
+		cout << "impar " << endl;
+	}
