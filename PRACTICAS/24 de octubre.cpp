e33549e7898fabef19b01889bacad26c28884ffa
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-24 11:53:48 -0600

    Create 24 de octubre.cpp

---
+++ PRACTICAS/24 de octubre.cpp
@@ -0,0 +1,32 @@
+#include iostream;
+
+using namespace std;
+
+int main()
+{
+  double peso, estutura;
+  double mc;
+  cout << "Ingresa tu peso: ";
+  cin >> peso;
+  cout << "Ingresa tu estura: ";
+  cin>> estatura;
+  cout << endl;
+
+  cout << fixed; // par precision de datos
+  cout << setprecision(2);
+  mc = peso / (estatura * estatura);
+  cout << "MC: " << mc << endl;
+  if(mc < 18.5)
+  {
+    cout << "Tu peso es bajo" << endl;
+  }
+  if(mc >= 18.5 and mc < 25)
+  {
+    cout << "Tu peso es normal" << endl;
+  }
+  else
+  {
+    cout << "Tu peso es mayor" << endl;
+  }
+  
+}
