577cc52b662d1c3775a14d05c07248de6c94bddd
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-24 12:02:11 -0600

    Update 24 de octubre.cpp

--- PRACTICAS/24 de octubre.cpp
+++ PRACTICAS/24 de octubre.cpp
@@ -1,32 +1,29 @@
-#include iostream;
+#include <iostream>;
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-  double peso, estutura;
-  double mc;
-  cout << "Ingresa tu peso: ";
-  cin >> peso;
-  cout << "Ingresa tu estura: ";
-  cin>> estatura;
-  cout << endl;
+    double peso, estatura;
+    double mc;
+    cout << "Ingresa tu peso: ";
+    cin >> peso;
+    cout << "Ingresa tu estura: ";
+    cin >> estatura;
+    cout << endl;
 
-  cout << fixed; // par precision de datos
-  cout << setprecision(2);
-  mc = peso / (estatura * estatura);
-  cout << "MC: " << mc << endl;
-  if(mc < 18.5)
-  {
-    cout << "Tu peso es bajo" << endl;
-  }
-  if(mc >= 18.5 and mc < 25)
-  {
-    cout << "Tu peso es normal" << endl;
-  }
-  else
-  {
-    cout << "Tu peso es mayor" << endl;
-  }
-  
+    cout << fixed; // par precision de datos
+    cout << setprecision(2);
+    mc = peso / (estatura * estatura);
+    cout << "MC: " << mc << endl;
+    if (mc < 18.5)
+    {
+        cout << "Tu peso es bajo" << endl;
+    }
+    if (mc >= 18.5 and mc < 25)
+    {
+        cout << "Tu peso es normal" << endl;
+    }
+
 }
