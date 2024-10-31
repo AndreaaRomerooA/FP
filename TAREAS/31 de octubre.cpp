3c628ba621b754848e0882922e322630d2988c06
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-10-31 12:32:48 -0600

    Create 31 de octubre.cpp

---
+++ TAREAS/31 de octubre.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3;
+    cout << "Ingrese el primer numero: ";
+    cin >> n1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> n2;
+    cout << "Ingrese el tercer numero :";
+    cin >> n3;
+    cout << "Numero: " << 
+    
+    // ) Si un ángulo es igual a 90 grados, imprimir el mensaje "El ángulo es un ángulo
+    // recto" sino imprimir el mensaje "El ángulo no es un ángulo recto". 
+
+    float grados;
+    cout << "Ingrese los grados: ";
+    cin >> grados;
+    if (grados == 90)
+    {
+        cout << "El ángulo es un ángulo recto" << endl;
+    }
+    else
+    {
+        cout << "El ángulo no es un ángulo recto" << endl;
+    }
+
+
+    // Si la temperatura es superior a 100 grados, visualizar el mensaje “por encima
+    //del punto de ebullición del agua” sino visualizar el mensaje “por debajo del
+      //  punto de ebullición del agua”.
+
+    int temp;
+    cout << "Ingrese la temperatura: ";
+    cin >> temp;
+    if (temp > 100)
+    {
+        cout << "Por encima del punto de ebullicion del agua" << endl;
+    }
+    else
+    {
+        cout << "Por debajo del punto de ebullicion del agua" << endl;
+    }
+
+
+    // Recibir un número, Si este número es positivo, sumar el número a total de
+    // positivos usando una variable que acumule los números negativos, sino sumar al
+       // total de negativos usando una variable que acumule los números negativos.
+
+    int num;
+    cout << "Ingrese un numero: " << endl;
+
+
+
+    // 
+
+
+
+
+
+}
