baa98394e6e4f3fd02e62f0676148bd90085c13e
AndreaaRomerooA <andrea.romero35@my.unitec.edu.mx>
2024-11-07 22:59:59 -0600

    Update tarea.cpp

--- TAREAS/tarea.cpp
+++ TAREAS/tarea.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream> 
     using namespace std;
 
     int main()
@@ -15,7 +19,7 @@
         cout << endl;
 
 
-        // EJERCICIO
+        // EJERCICIO 1.22
         int numero1, numero2, total;
         string opcion2;
         do
@@ -57,15 +61,95 @@
             if (opcion2 == "e")
             {
                 cout << "Resultado de operacion: " << (numero1 % numero2) << endl;
+                cout << endl;
             }
         } while (opcion2 != "f");
         break;
 
 
         // EJERCICIO 1.23
-        long long 
+        long long numTarjeta, dinero;
+        string opcion3;
+        cout << "Ingrese su numero de tarjeta: ";
+        cin >> numTarjeta;
+        cout << "Ingrese su pin: ";
+        if (numTarjeta != "1234")
+        {
+            cout << endl;
+        }
+        do
+            {
+                if (numTarjeta == "1234")
+                {
+                cout << "Menu de operaciones cajero Unitec" << endl;
+                cout << endl;
+                cout << "a) Consulta de saldo" << endl;
+                cout << "b) Retiro de dinero" << endl;
+                cout << "c) Cancelar operacion" << endl;
+                } else 
+                { 
+                    cout << "Pin incorrecto" << endl;
+                }
+                cout << endl;
+                cin >> opcion3;
 
-       
-    
+                if (opcion3 == "a")
+                {
+                    cout << "Saldo: $3450.50" << endl;
+                    cout << endl;
+                }
+                if (opcion3 == "b")
+                {
+                    cout << "Dinero a retirar: ";
+                    cin >> dinero;
+                    if (dinero > 3450)
+                    { 
+                        cout << "Fondos insuficientes" << endl;
+                        cout << endl;
+                    }
+                    else 
+                    { 
+                        cout << "Saldo remanente: $" << (3450.50 - dinero) << endl;
+                        cout << endl;
+                    }
+                }   
+            }
+            while (opcion3 != "c");
+        break;
+        cout << endl;
+
+
+        // EJERCICIO 1.24
+        srand(time(NULL));
+	int castigo = 0;
+	int vida=12;
+	int premio = 0;
+	cout << "vida inicial " 
+		<< vida << endl;
+	while (vida > 0) 
+	{
+		//deducir uno vida
+		vida--;
+		cout << "costo de ciclo " 
+			<< vida << endl;
+		// castigo?
+		castigo = rand() % 2;
+		if (castigo == 1) 
+		{
+			vida = vida - 3;
+			cout << "castigo "
+				<< vida << endl;
+		}
+		premio = rand() % 2;
+		if (premio ==1)
+		{
+			vida = vida + 3;
+			cout << "premio "
+				<< vida << endl;
+		}
+		cout << "vida despues ciclo "
+			<< vida << endl;
+	}
+        
     
     }
